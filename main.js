a85ae7a1e86115c1db93715fa646e22956911461
Manas verma <manasverma1357@gmail.com>
2025-01-24 16:18:15 +0530

    Add func for taking input from user

--- main.js
+++ main.js
@@ -2,14 +2,32 @@
 function getComputerChoice(){
     let comp = Math.floor(Math.random() * 3)
     if (comp == 0){
-        return "ROCK";
+        return "Computer have selected ROCK";
     }
     else if (comp == 1){
-        return "PAPER";
+        return "Computer have selected PAPER";
     }
     else if (comp == 2){
-        return "SCISSOR";
+        return "Computer have selected SCISSOR";
     }
     }
 
+//Function for getting Human's choice 
+function getHumanChoice(){
+    let hum = prompt("Enter your choice:")
+    if (hum.toLowerCase() == "rock") {
+        return "You have selected Rock";
+    }
+    else if (hum.toLowerCase() == "paper") {
+        return "You have selected Paper";
+    }
+    else if (hum.toLowerCase() == "scissor") {
+        return "You have selected Scissor";
+    }
+    else {
+        return "Invalid Input";
+    }
+}
+
+console.log(getHumanChoice())
 console.log(getComputerChoice())
